ae3945746b6857250c18c3c38f41e3cf06531242
arpabanerjee <arpa.banerjee.uttarpara@gmail.com>
2020-04-27 18:49:39 +0530

    Swagger2 annotaions for department controller

--- department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -15,8 +15,15 @@ import com.example.departmentservice.dao.Department;
 import com.example.departmentservice.exception.DeptNotFoundException;
 import com.example.departmentservice.service.DepartmentService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+
 @RestController
 @RequestMapping("/api/v1")
+@Api(value="Department Controller")
 public class DepartmentController {
 	
 	private final Logger log = LoggerFactory.getLogger(this.getClass().getName());
@@ -24,22 +31,41 @@ public class DepartmentController {
 	@Autowired
 	DepartmentService departmentService;
 	
+	@ApiOperation(value = "View a list of available departments", response = ResponseEntity.class)
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "Successfully retrieved all departments"),
+	    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	})
 	@GetMapping("/getAllDepartments")
 	public ResponseEntity<List<Department>> getAllDepartments() {
 		return ResponseEntity.ok().body(departmentService.getAllDepartments());
 	}
 	
+	@ApiOperation(value = "Get a department based on dept num", response = ResponseEntity.class)
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "Successfully retrieved department object based on departmet number"),
+	    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	})
 	@GetMapping("/getDepartmentByDeptNumber/{deptNum}")
-	public ResponseEntity<Department> getDepartmentByDeptNumber(@PathVariable String deptNum) throws DeptNotFoundException{
+	public ResponseEntity<Department> getDepartmentByDeptNumber(
+			@ApiParam(value = "Department num from which department object will retrieve", required = true) 
+			@PathVariable String deptNum) 
+					throws DeptNotFoundException {
 		Department department = departmentService.getDepartmentById(deptNum);
 		if(log.isDebugEnabled()) {
 			log.debug("Department found: {}", department);
 		}
 		return ResponseEntity.ok().body(department);
 	}
-	
+	@ApiOperation(value = "Get a department based on employee id", response = ResponseEntity.class)
+	@ApiResponses(value = {
+	    @ApiResponse(code = 200, message = "Successfully retrieved department object based on employee id"),
+	    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+	})
 	@GetMapping("/getDepartmentByEmpId/{empId}")
-	public ResponseEntity<Department> getDepartmentByEmpId(@PathVariable long empId) throws DeptNotFoundException{
+	public ResponseEntity<Department> getDepartmentByEmpId(
+			@ApiParam(value = "Employee id from which department object will be retrieved", required = true) 
+			@PathVariable long empId) throws DeptNotFoundException{
 		Department department = departmentService.getDepartmentByEmpId(empId);
 		if(log.isDebugEnabled()) {
 			log.debug("Department found: {}", department);
